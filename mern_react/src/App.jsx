f2e0ce91c9a061ef55fd5821d87af9770d42f1f1
Ashwans7 <ashwanshrestha.kun@gmail.com>
2025-06-30 23:11:24 +0545

    Hooks in react

--- mern_react/src/App.jsx
+++ mern_react/src/App.jsx
@@ -1,24 +1,38 @@
 
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route, RouterProvider} from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
+import { useState } from 'react'
+
 
 
 
 function App() {
 
+     const [count, setCount] = useState(0); // count is state, 0 is initial value
+     const [name,setName] = useState("Ashwan")
 
   return (
-   <BrowserRouter>
-   <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path ='/about' element={<About />}/>
-      <Route path ='/contact' element={<Contact />}/>
-   </Routes>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={()=> setCount(count - 1)}> - </button>
+      <h1>{name}</h1>
+      <p>Hello World!</p>
+      <button onClick={()=> setName("Ashwan Shrestha")}>Change Name</button>
+    </div>
+  
+  //  <BrowserRouter>
+  //  <Routes>
+  //     <Route path='/' element={<Home />} />
+  //     <Route path ='/about' element={<About />}/>
+  //     <Route path ='/contact' element={<Contact />}/>
+  //  </Routes>
 
-   </BrowserRouter>
+  //  </BrowserRouter>
   )
 }
+
 
 export default App
