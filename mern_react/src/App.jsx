800a271d5a072475eab15dae93cfaa0b75507488
Ashwans7 <ashwanshrestha.kun@gmail.com>
2025-07-01 14:20:14 +0545

    useEffect and its types

--- mern_react/src/App.jsx
+++ mern_react/src/App.jsx
@@ -1,27 +1,62 @@
 
-import {BrowserRouter,Routes,Route, RouterProvider} from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-import { useState } from 'react'
+// import {BrowserRouter,Routes,Route, RouterProvider} from 'react-router-dom'
+// import Home from './Home'
+// import About from './About'
+// import Contact from './Contact'
+// import { useState } from 'react'
+
+import { useEffect, useState } from "react"
 
 
 
 
 function App() {
 
-     const [count, setCount] = useState(0); // count is state, 0 is initial value
-     const [name,setName] = useState("Ashwan")
+  const [count,setCount] = useState(0)
+  const [nextCount,setnextCount] = useState(0)
+
+  //useEffect
+//   //first type of useEffect
+//   useEffect(()=>{
+//     console.log("first useEffect")
+    
+
+//   },[])
+
+// // Second Type of useEffect
+// useEffect(()=>{
+//   // document.title = "learning useEffect" + count
+//   console.log("useEffect was called")
+// },[count])
+
+//Third type of useEffect
+// useEffect(()=>{
+//   console.log("Third useEffect was called")
+// })
+
+
+//useState
+//useState hook
+    //  const [count, setCount] = useState(0); // count is state, 0 is initial value
+    //  const [name,setName] = useState("Ashwan")
 
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={()=> setCount(count - 1)}> - </button>
-      <h1>{name}</h1>
-      <p>Hello World!</p>
-      <button onClick={()=> setName("Ashwan Shrestha")}>Change Name</button>
-    </div>
+    <>
+    <h1>Learning useEffect</h1>
+    <h2>{count}</h2>
+    <button onClick={()=>setCount(count+1)}>+ </button>
+    <h1>{nextCount}</h1>
+    <button onClick={()=>setnextCount(nextCount + 1)}>+ </button>
+    </>
+    
+    // <div>
+    //   <p>You clicked {count} times</p>
+    //   <button onClick={() => setCount(count + 1)}>+</button>
+    //   <button onClick={()=> setCount(count - 1)}> - </button>
+    //   <h1>{name}</h1>
+    //   <p>Hello World!</p>
+    //   <button onClick={()=> setName("Ashwan Shrestha")}>Change Name</button>
+    // </div>
   
   //  <BrowserRouter>
   //  <Routes>
